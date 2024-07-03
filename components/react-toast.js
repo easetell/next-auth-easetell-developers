6a01ff22aba7915d5685862fa8c674a90f1a3d75
Easetell <109558276+easetell@users.noreply.github.com>
2024-07-03 21:51:19 +0300

    02

---
+++ components/react-toast.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function ReactToast() {
+    return (
+        <div>
+            <ToastContainer />
+        </div>
+    );
+}
